1e6e91cca7166eff265112a7e8dae18f2e6bbd0b
Piyushkumar99 <k.piyushkumar2000@gmail.com>
2021-06-04 21:16:20 +0700

    Updates

---
+++ precache-manifest.f230671405964ccd2d468322a94051e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2283a89daedf73c93f4d832d92fe0b",
+    "url": "/COVID-19_TRACKER/index.html"
+  },
+  {
+    "revision": "f58a1c7d279bb6bd3da5",
+    "url": "/COVID-19_TRACKER/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "6b5c401c09d691964378",
+    "url": "/COVID-19_TRACKER/static/css/main.b5c4c3c5.chunk.css"
+  },
+  {
+    "revision": "f58a1c7d279bb6bd3da5",
+    "url": "/COVID-19_TRACKER/static/js/2.7f96593f.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/COVID-19_TRACKER/static/js/2.7f96593f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5c401c09d691964378",
+    "url": "/COVID-19_TRACKER/static/js/main.a3aee651.chunk.js"
+  },
+  {
+    "revision": "934c41f57d9af746986e",
+    "url": "/COVID-19_TRACKER/static/js/runtime-main.805dd37e.js"
+  }
+]);
